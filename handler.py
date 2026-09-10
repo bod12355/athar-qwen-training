73f047f19710caefc0b26335491802d914a04691
da02bod-art <da02.bod@gmail.com>
2026-09-10 14:20:03 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -346,21 +346,21 @@ def handler(job):
         flush=True
     )
 
-if job_input.get("preflight", False):
-    adapter_file = os.path.join(
-        checkpoint_path,
-        "adapter_model.safetensors"
-    )
+    if job_input.get("preflight", False):
+        adapter_file = os.path.join(
+            checkpoint_path,
+            "adapter_model.safetensors"
+        )
 
-    return {
-        "status": "preflight_ok",
-        "training_type": training_type,
-        "checkpoint": checkpoint_path,
-        "adapter_size_mb": round(
-            os.path.getsize(adapter_file) / 1024 / 1024,
-            2
-        ),
-    }
+        return {
+            "status": "preflight_ok",
+            "training_type": training_type,
+            "checkpoint": checkpoint_path,
+            "adapter_size_mb": round(
+                os.path.getsize(adapter_file) / 1024 / 1024,
+                2
+            ),
+        }
     
     os.makedirs(
         info["output_dir"],
